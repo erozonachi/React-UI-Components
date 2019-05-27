26235997cf34c9d797c083bf2b19bb60a813cc43
erozonachi <erozonachi.james@gmail.com>
2019-05-27 15:17:12 +0100

    Create a card container component
    Create a `<CardContainer/ >` container component that will hold your
     card components. The entire `<CardContainer />` should take a user
      to `https://www.reactjs.org` when clicked.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import './Card.css';
+import './CardBanner';
+import './CardContent';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const cardInfo = {
+  url: 'https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png',
+  heading: 'Get started with React',
+  text: 'React makes it painless to create interactive UIs. Design simple views for each state in your application.',
+  reactURL: 'reactjs.org',
+};
+const CardContainer = (props) => (
+  <a 
+    href={props.url}
+  >
+   <CardBanner url={cardInfo.url} />
+   <CardContent heading={cardInfo.heading} text={cardInfo.text} reactURL={cardInfo.reactURL} />
+  </a>
+);
+
+export default CardContainer;
