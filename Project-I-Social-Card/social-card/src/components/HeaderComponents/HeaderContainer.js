c64a3025fe7ee27f3023188136680bf2a0937920
erozonachi <erozonachi.james@gmail.com>
2019-05-27 18:57:26 +0100

    Format header timestamp with momentjs library

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -6,12 +6,13 @@ import './HeaderContent';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
 import HeaderContent from './HeaderContent';
+import Moment from 'moment';
 
 const headerInfo = {
   url: 'https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png',
   title: 'Lambda School',
   handle: '@LambdaSchool',
-  timeStamp: '26 jan',
+  timeStamp: Moment().format('DD MMM'),
   text: 'Let\'s learn React by building simple interfaces with components. Don\'t try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React.',
 }
 
--- README.md
+++ README.md
@@ -53,7 +53,7 @@
 - [x] Create a `<Footer/>` component that pulls in the icons and displays them properly.
   - [x] Build out the functionality so that a user can click on the icons and have them react to events.
 - [x] Ensure that your product is as pixel perfect as possible using any tools that you were introduced to in previous weeks.
-- Look up the `moment.js` library and figure out how to format your `time-stamp` in your header to be todays date.
+- [x] Look up the `moment.js` library and figure out how to format your `time-stamp` in your header to be todays date.
 
 ## _Project II - React Calculator_
 
